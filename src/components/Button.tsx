0633b1fbda089294e9a4ee23cdf18c07762c30de
IgorStumpf <igorluizstumpf@gmail.com>
2021-08-16 16:16:10 -0300

    "Home, Button, Css and icons creation"

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,4 @@
-type ButtonProps = {
+/* type ButtonProps = {
     text?: string;
 }
 
@@ -8,4 +8,22 @@ export function Button(props: ButtonProps) {
     )
 }
 
-// O caracter de interrogação, significa que o text é opcional
+// O caracter de interrogação, significa que o text é opcional */
+
+import { ButtonHTMLAttributes } from 'react'
+import "../styles/button.scss"
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
+export function Button(props: ButtonProps) {
+    return(
+        <button className="button" {...props} />
+    )
+}
+
+/* eu preciso/quero passar para o botão qualquer tipo de atributo que eu passaria para o HTML
+O próprio React exporta para uso uma tipagem do TypeScript chamada ButtonHTMLAttributes (clicando com o botão direito nele, dá para ver todos esses atributos)
+Ele permite que todos os atributos possam ser usados
+Quando chamar a funcão <Button /> e apertar um CTRL + Espaço, ele vai passar todas as propriedades disponíveis para uso no botão
+
+*/
